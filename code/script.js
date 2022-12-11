61e5d9a13bfff86138bf22fd45a5be38b24d5415
Lotta <charlotte.a.nyman@gmail.com>
2022-12-11 16:20:54 +0100

    fixed the name-input and reset the chat

--- code/script.js
+++ code/script.js
@@ -5,13 +5,11 @@ const form = document.getElementById("name-form");
 const input = document.getElementById("name-input");
 const sendBtn = document.getElementById("send");
 
+let name = "";
+
 // If you need any global variables that you can use across different functions, declare them here:
 
 let questionNr = 0;
-
-const userName = (msg) => {
-  showMessage(msg, "user");
-};
 
 // Declare your functions after this comment
 
@@ -38,7 +36,7 @@ const showMessage = (message, sender) => {
       </section>
     `;
   }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
+
   chat.scrollTop = chat.scrollHeight;
 };
 
@@ -47,7 +45,7 @@ const nextQuestion = (event) => {
   questionNr++;
   // console.log("our currentQuestion variable is:", questionNr);
   if (questionNr === 1) {
-    handelNameQuestion();
+    name = handelNameQuestion();
     setTimeout(coffeeQuestion, 1500);
   } else if (questionNr === 2) {
     handelNameQuestion();
@@ -55,17 +53,17 @@ const nextQuestion = (event) => {
   } else if (questionNr === 3) {
     handelNameQuestion();
     setTimeout(coffeeQuestion, 1000);
-  }
+    console.log("lattebutton3");
+  } 
 };
 
 // Starts here
 const greeting = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
   showMessage("Hello there, What's your name?", "bot");
-  // Just to check it out, change 'bot' to 'user' here 👆
 };
 
-const coffeeQuestion = (name) => {
+const coffeeQuestion = () => {
   showMessage(`Welcome ${name} do you like to order a coffee?`, "bot");
   input.value = "";
 };
@@ -82,45 +80,47 @@ const question2 = () => {
   document
     .getElementById("lBtn")
     .addEventListener("click", () => ShowCoffee("Latte"));
+  console.log("lattebutton");
   document
     .getElementById("fBtn")
     .addEventListener("click", () => ShowCoffee("Frappe"));
+  console.log("frappebutton");
   document
     .getElementById("eBtn")
     .addEventListener("click", () => ShowCoffee("Espresso"));
+  console.log("espresobutton");
 };
 
 const ShowCoffee = (type) => {
   // questionNr++;
   if (type === "Latte") {
-    showMessage(`One Latte is coming up! `,"bot");
+    showMessage(`Thank you ${name},One Latte is coming up! `, "bot");
+    inputWrapper.innerHTML = ``
+    
+    console.log("lattebutton2");
   } else if (type === "Frappe") {
-    showMessage(`one Frappe is coming up!`,"bot");
-  } else type === "Espresso";
-  showMessage(`one Frappe is coming up! `, "bot");
+    showMessage(`Thank you ${name},one Frappe is coming up!`, "bot");
+    inputWrapper.innerHTML = ``
+
+    console.log("frappebutton2");
+  } else if (type === "Espresso") {
+    showMessage(`Thank you ${name}, one Frappe is coming up! `, "bot");
+    inputWrapper.innerHTML = ``
+  }
 };
+
+// const endQuestion = () => {
+//   showMessage(`Thank you ${name} and good bye`)
+// }
+ 
 
 const handelNameQuestion = () => {
   const name = input.value;
   showMessage(name, "user");
   input.value = "";
+  return name;
 };
 
-// const ShowCoffee = () => {
-//   if (latteButton) {
-//     showMessage("one Latte is coming up! ");
-//   } else if (frappeButton) {
-//     showMessage("one Frappe is coming up! ");
-//   } else espressoButton;
-//   showMessage("one Frappe is coming up! ");
-// };
-// Set up your eventlisteners here
 form.addEventListener("submit", nextQuestion);
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 1000);
